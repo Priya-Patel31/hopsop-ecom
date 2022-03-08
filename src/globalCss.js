7b356c16a5b04a4d90cc36194c195b57ca2e6c6c
Priya-Patel31 <patel3112priya@gmail.com>
2022-03-08 15:32:23 +0530

    fix: fixed some bug

--- src/globalCss.js
+++ src/globalCss.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as globalStyles from "./global.module.css";
 
-export default function global(){
-return <section className={globalStyles}></section>
+export default function GlobalCSS() {
+  return <section className={globalStyles}></section>;
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {GlobalCSS} from "./globalCss"
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import GlobalCSS from "./globalCss";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalCSS>
-    <App />
+      <App />
     </GlobalCSS>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
