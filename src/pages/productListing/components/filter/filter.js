ebe8cffdd04fb17a54a0e0856f778d83269da9b8
Priya-Patel31 <patel3112priya@gmail.com>
2022-03-13 17:02:34 +0530

    fix : minor changes

--- src/pages/productListing/components/filter/filter.js
+++ src/pages/productListing/components/filter/filter.js
@@ -1,4 +1,4 @@
-import categoryData  from "../../../../temp/category.json";
+import categoryData from "../../../../temp/category.json";
 import ratingData from "../../../../temp/rating.json";
 
 const Filter = () => {
@@ -15,13 +15,23 @@ const Filter = () => {
         <p className="mt-2 font-bold text-xs">SORT</p>
         <ul className="filter-type">
           <li className="my-2 list-style-none">
-            <input type="radio" value="price-high-to-low" name="price" id="price-high-to-low" />
+            <input
+              type="radio"
+              value="price-high-to-low"
+              name="price"
+              id="price-high-to-low"
+            />
             <label htmlFor="price-high-to-low" className="text-xs">
               Price High to low
             </label>
           </li>
           <li className="mb-2 list-style-none">
-            <input type="radio" value="price-low-to-high" name="price" id="price-low-to-high" />
+            <input
+              type="radio"
+              value="price-low-to-high"
+              name="price"
+              id="price-low-to-high"
+            />
             <label htmlFor="price-low-to-high" className="text-xs">
               Price Low to high
             </label>
@@ -33,7 +43,12 @@ const Filter = () => {
           {categoryData.categories.map((category) => {
             return (
               <li key={category.id} className="mb-2 list-style-none">
-                <input type="checkbox" name={category.name} checked={true} id={category.name} />
+                <input
+                  type="checkbox"
+                  name={category.name}
+                  checked={true}
+                  id={category.name}
+                />
                 <label htmlFor={category.name} className="text-xs">
                   {category.name}
                 </label>
@@ -47,9 +62,14 @@ const Filter = () => {
           {ratingData.ratings.map((rating) => {
             return (
               <li key={rating.id} className="mb-2 list-style-none">
-                <input type="checkbox" name={rating.name} checked={true} id={rating.name}/>
+                <input
+                  type="checkbox"
+                  name={rating.name}
+                  checked={true}
+                  id={rating.name}
+                />
                 <label htmlFor={rating.name} className="text-xs">
-                 {rating.name}
+                  {rating.name}
                 </label>
               </li>
             );
@@ -59,4 +79,4 @@ const Filter = () => {
     </div>
   );
 };
-export default  Filter ;
+export default Filter;
