b0ab9e1c4253f0b0255ea8f18e53ebb0fe63b1dc
Priya-Patel31 <patel3112priya@gmail.com>
2022-03-19 15:57:18 +0530

    refactor : removed unnecessary code

--- src/services/product_services.js
+++ src/services/product_services.js
@@ -1,30 +1,6 @@
 // fetching products
-import axios from "axios";
-import fetchApi from "../shared/custom-hooks/useAxios";
+import fetchApi from "../shared/utils/fetchApi";
 import { brands } from "../temp/brands.json";
-
-// export const fetchProducts = async () => {
-//   try {
-//     const {
-//       data: { products },
-//     } = await axios.get("/api/products");
-//     const {
-//       data: { categories },
-//     } = await axios.get("/api/categories");
-
-//     return {
-//       data: { products, categories, brands },
-//       success: true,
-//       message: "products fetched successfully",
-//     };
-//   } catch (e) {
-//     return {
-//       data: null,
-//       success: false,
-//       message: "products failed to fetch",
-//     };
-//   }
-// };
 
 export const fetchProducts = async() => {
    const { data, success } = await fetchApi({
--- src/shared/utils/fetchApi.js
+++ src/shared/utils/fetchApi.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 async function fetchApi({ url, method, body, propertyName }) {
-  const token =
+  const defaultToken =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIxN2I2MDZjMS05OTk3LTRlNWYtODBmMC0xYWEyN2VlOGE1NTMiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20iLCJpYXQiOjE2NDc2MjgwODR9.CcWEd_xGSx_T1lCyeyyys4JLCVuWR5ThmVVtgi0qWmE";
-
+   const token =  localStorage.getItem("token")??defaultToken
 
   try {
     const { data } = await axios({
